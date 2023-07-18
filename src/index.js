5727fb4fac058d7952530aea8ff4776ae2fc6ffe
ChrisTorng <ChrisTorng@hotmail.com>
2023-07-18 15:56:44 +0800

    Add all

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const guitar = document.getElementById('guitar');
 const bass = document.getElementById('bass');
 const drum = document.getElementById('drum');
 const allParts = [vocal, other, piano, guitar, bass, drum];
-const partVolumeTemplate = document.getElementById('part-volume-template');
+const HAVE_ENOUGH_DATA = 4;
 main();
 function main() {
-    allParts.forEach(audio => audio.load());
+    setEvents();
     setPartsVolume('allParts', '全部');
     setPartsVolume('vocal', '人聲');
     setPartsVolume('other', '其他');
@@ -17,7 +17,14 @@ function main() {
     setPartsVolume('guitar', '吉它');
     setPartsVolume('bass', '貝斯');
     setPartsVolume('drum', '鼓　');
-    setEvents();
+}
+function loadSong(song) {
+    allParts.forEach(audio => {
+        audio.src = `./songs/${song}/${audio.id}.mp3`;
+        audio.load();
+        audio.volume = 0.25;
+    });
+    return false;
 }
 function setPartsVolume(id, title) {
     const titleHtml = `<span id="${id}">${title}</span>`;
@@ -31,16 +38,65 @@ function setPartsVolume(id, title) {
 `;
 }
 function getVolumeRadio(id, volume, selected = false) {
-    return `<input type="radio" name="${id}-radio" id="${id}${volume}"
+    return `<input type="radio" name="${id}-radio" id="${id}${volume}" disabled="disabled"
         onclick="setVolume(${id}, ${volume})" ${selected ? 'checked' : ''}
         /><label id="${id}${volume}-label" for="${id}${volume}" class="part">${volume} </label>`;
 }
 function setEvents() {
-    document.getElementById('play').addEventListener('click', () => {
-        allParts.forEach(audio => audio.play());
+    document.getElementById('playOrPause').addEventListener('click', () => {
+        if (allParts[0].paused) {
+            allParts.forEach(audio => {
+                if (audio.readyState === HAVE_ENOUGH_DATA) {
+                    audio.play();
+                }
+            });
+            document.getElementById('playOrPause').innerHTML = '║ ';
+        }
+        else {
+            allParts.forEach(audio => {
+                if (audio.readyState === HAVE_ENOUGH_DATA) {
+                    audio.pause();
+                }
+            });
+            document.getElementById('playOrPause').innerHTML = '▶';
+        }
     });
-    document.getElementById('pause').addEventListener('click', () => {
-        allParts.forEach(audio => audio.pause());
+    allParts.forEach(audio => audio.oncanplaythrough = function () {
+        console.log(audio.id, 'oncanplaythrough');
+        document.getElementById(`${audio.id}0`).disabled = false;
+        document.getElementById(`${audio.id}25`).disabled = false;
+        document.getElementById(`${audio.id}50`).disabled = false;
+        document.getElementById(`${audio.id}75`).disabled = false;
+        document.getElementById(`${audio.id}100`).disabled = false;
+        document.getElementById(`allParts0`).disabled = false;
+        document.getElementById(`allParts25`).disabled = false;
+        document.getElementById(`allParts50`).disabled = false;
+        document.getElementById(`allParts75`).disabled = false;
+        document.getElementById(`allParts100`).disabled = false;
+    });
+    allParts.forEach(audio => audio.onerror = function () {
+        switch (audio.error.code) {
+            case 1:
+                console.log(audio.id, 'MEDIA_ERR_ABORTED');
+                break;
+            case 2:
+                console.log(audio.id, 'MEDIA_ERR_NETWORK');
+                break;
+            case 3:
+                console.log(audio.id, 'MEDIA_ERR_DECODE');
+                break;
+            case 4:
+                console.log(audio.id, 'MEDIA_ERR_SRC_NOT_SUPPORTED');
+                break;
+            default:
+                console.log(audio.id, 'UNKNOWN_ERROR');
+                break;
+        }
+        document.getElementById(`${audio.id}0`).disabled = true;
+        document.getElementById(`${audio.id}25`).disabled = true;
+        document.getElementById(`${audio.id}50`).disabled = true;
+        document.getElementById(`${audio.id}75`).disabled = true;
+        document.getElementById(`${audio.id}100`).disabled = true;
     });
 }
 function setVolume(target, volume) {

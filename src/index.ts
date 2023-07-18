7359090b0af28ab477da4c2664b92be1d44cf6bf
ChrisTorng <ChrisTorng@hotmail.com>
2023-07-18 15:55:26 +0800

    Parts enable/disable Combine Play/Pause button and use icon Add songs with switch

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ const bass = document.getElementById('bass') as HTMLAudioElement;
 const drum = document.getElementById('drum') as HTMLAudioElement;
 const allParts = [vocal, other, piano, guitar, bass, drum];
 
-const partVolumeTemplate = document.getElementById('part-volume-template') as HTMLTemplateElement;
+const HAVE_ENOUGH_DATA = 4;
 
 main();
 
 function main(): void {
-    allParts.forEach(audio => audio.load());
-
+    setEvents();
+    
     setPartsVolume('allParts', '全部');
     setPartsVolume('vocal', '人聲');
     setPartsVolume('other', '其他');
@@ -20,8 +20,15 @@ function main(): void {
     setPartsVolume('guitar', '吉它');
     setPartsVolume('bass', '貝斯');
     setPartsVolume('drum', '鼓　');
+}
 
-    setEvents();
+function loadSong(song: string): boolean {
+    allParts.forEach(audio => {
+        audio.src = `./songs/${song}/${audio.id}.mp3`;
+        audio.load();
+        audio.volume = 0.25;
+      });
+    return false;
 }
 
 function setPartsVolume(id: string, title: string): void {
@@ -38,19 +45,70 @@ function setPartsVolume(id: string, title: string): void {
 }
 
 function getVolumeRadio(id: string, volume: number, selected: boolean = false): string {
-    return `<input type="radio" name="${id}-radio" id="${id}${volume}"
+    return `<input type="radio" name="${id}-radio" id="${id}${volume}" disabled="disabled"
         onclick="setVolume(${id}, ${volume})" ${selected ? 'checked' : ''}
         /><label id="${id}${volume}-label" for="${id}${volume}" class="part">${volume} </label>`;
 }
 
 function setEvents(): void {
-    document.getElementById('play')!.addEventListener('click', () => {
-        allParts.forEach(audio => audio.play());
+    document.getElementById('playOrPause')!.addEventListener('click', () => {
+        if (allParts[0].paused) {
+          allParts.forEach(audio => {
+            if (audio.readyState === HAVE_ENOUGH_DATA) {
+              audio.play();
+            }
+          });
+          document.getElementById('playOrPause')!.innerHTML = '║ ';
+        } else {
+          allParts.forEach(audio => {
+            if (audio.readyState === HAVE_ENOUGH_DATA) {
+              audio.pause();
+            }
+          });
+          document.getElementById('playOrPause')!.innerHTML = '▶';
+        }
     });
 
-    document.getElementById('pause')!.addEventListener('click', () => {
-        allParts.forEach(audio => audio.pause());
-    });
+    allParts.forEach(audio => audio.oncanplaythrough = function() {
+      console.log(audio.id, 'oncanplaythrough');
+      (document.getElementById(`${audio.id}0`)! as HTMLInputElement).disabled = false;
+      (document.getElementById(`${audio.id}25`)! as HTMLInputElement).disabled = false;
+      (document.getElementById(`${audio.id}50`)! as HTMLInputElement).disabled = false;
+      (document.getElementById(`${audio.id}75`)! as HTMLInputElement).disabled = false;
+      (document.getElementById(`${audio.id}100`)! as HTMLInputElement).disabled = false;
+      (document.getElementById(`allParts0`)! as HTMLInputElement).disabled = false;
+      (document.getElementById(`allParts25`)! as HTMLInputElement).disabled = false;
+      (document.getElementById(`allParts50`)! as HTMLInputElement).disabled = false;
+      (document.getElementById(`allParts75`)! as HTMLInputElement).disabled = false;
+      (document.getElementById(`allParts100`)! as HTMLInputElement).disabled = false;
+  });
+
+    allParts.forEach(audio => audio.onerror = function() {
+        switch(audio.error!.code) {
+          case 1:
+            console.log(audio.id, 'MEDIA_ERR_ABORTED');
+            break;
+          case 2:
+            console.log(audio.id, 'MEDIA_ERR_NETWORK');
+            break;
+          case 3:
+            console.log(audio.id, 'MEDIA_ERR_DECODE');
+            break;
+          case 4:
+            console.log(audio.id, 'MEDIA_ERR_SRC_NOT_SUPPORTED');
+            break;
+          default:
+            console.log(audio.id, 'UNKNOWN_ERROR');
+            break;
+        }
+
+        (document.getElementById(`${audio.id}0`)! as HTMLInputElement).disabled = true;
+        (document.getElementById(`${audio.id}25`)! as HTMLInputElement).disabled = true;
+        (document.getElementById(`${audio.id}50`)! as HTMLInputElement).disabled = true;
+        (document.getElementById(`${audio.id}75`)! as HTMLInputElement).disabled = true;
+        (document.getElementById(`${audio.id}100`)! as HTMLInputElement).disabled = true;
+        });
+      
 }
 
 function setVolume(target: HTMLAudioElement | HTMLAudioElement[], volume: number): void {
